3d0ecc6b11816fabe9d40a80d8e3aeaab3d9caa6
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-18 10:19:07 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+
+const result=require('./result')
+result.add(2.3)
+result.squre(6)
+
---
+++ resualt.js
@@ -0,0 +1,12 @@
+function add(a,b){
+    console.log(a+b)
+}
+
+function squre(s){
+    console.log(s*s)
+}
+
+module.exports= {
+    add : add,
+    squre:squre
+}

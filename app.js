239ab9568fadcac9835796f873f75f5aa05867ad
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-18 14:00:51 -0800

    url

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const fs =require('fs');
+const url = require('url');
 
 const server = http.createServer((req,res )=>{
-    res.end('hello in first app')
+
+    const path = url.parse(req.url).pathname
+    console.log(path)
+
+    //read file >>3
+    fs.readFile('./index.html',null,(error,data)=>{
+        if (error){
+            res.end('file have error')
+        }else{
+            res.end(data)
+        }
+    })
 });
 
 server.listen(3000 , '127.0.0.1',()=>{

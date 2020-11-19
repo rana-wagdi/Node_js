4da5d7058de718ffb6bc353fa88294ef0decfef6
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-18 19:08:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,48 @@ const url = require('url');
 
 const server = http.createServer((req,res )=>{
 
-    const path = url.parse(req.url).pathname
-    console.log(path)
+const path = url.parse(req.url).pathname
 
-    //read file >>3
-    fs.readFile('./index.html',null,(error,data)=>{
-        if (error){
-            res.end('file have error')
-        }else{
-            res.end(data)
-        }
-    })
+switch(path){
+    case '/':
+        handlerequest('index.html',res)
+        break;
+    case '/any':
+        res.end('any page');
+
+        break;
+    case '/user':
+        res.end('user page')
+        break;
+
+    default:
+        res.end('this page not found');
+        break;
+}
+
 });
-
 server.listen(3000 , '127.0.0.1',()=>{
     console.log('server runing...')
 })
 
+function handlerequest(filepath,res){
+        //read file >>3
+        fs.readFile('./index.html',null,(error,data)=>{
+            if (error){
+                res.end('file have error')
+            }else{
+                res.end(data)
+            }
+        })
+    }
 
 
 
 
 
+
+
+/*
+*/
 
 

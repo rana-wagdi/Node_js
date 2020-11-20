cdc245abae203415846926f646ebc2d5f293420e
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-19 19:51:33 -0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,16 +1,27 @@
-let p =new Promise((resolve,reject)=>{
-    let a =1+2
-    if (a==2){
-        resolve('Success')
-    }else{
-        reject('failed')
-    }
-})
+const { response } = require("express")
 
-p.then((message)=>{
-    console.log('This is in the then '+message)
+function makeRequest(location) {
+    return new Promise ((resolve,reject)=> {
+        console.log(`Making request to ${location}`)
+        if (location=='Google'){
+            resolve('Google say hi')
+            } else {
+                reject('we can only talk to Google')
 
-}).catch((message)=> {
-    console.log('This is in the catch '+message)
+            }
+    })
+}
 
+function processRequest(response){
+    return new Promise((resolve,reject)=>{
+        console.log('Processing response')
+        resolve(`Extra Information +${response}`)
+    })
+}
+
+makeRequest('Google').then(response=>{
+    console.log('Response Received')
+    return processRequest(response)
+}).then(processedRequest => {
+    console.log(processedRequest)
 })

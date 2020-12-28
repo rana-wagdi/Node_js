2343426159650f1d328aadf6d397f677506ed488
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-12-28 04:31:54 -0800

    ..

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+projectData={};
 //Express to run server and routes
 const express =require('express');
 
 //Start up an instance of app
 const app = express();
 
+const jsonData ={count:12, message:'hey'};
+
+var fs = require('fs')
+app.get('/',function(req,res){
+  fs.readFile('index.html',function(err,buffer){
+
+
+    res.setHeader('Content-Type','text/html')
+    res.send(html);
+  })
+  //res.sendFile takes an absoulte path to a file and
+  //sets the mime type based n the file extname
+  //res.sendFile(__dirname+'/index.html',function(err){
+  //  if(err){
+    //  res.sendStatus(500).send(err);
+   // }
+  //})
+});
+
+app.get('/data',function(req, res){
+  res.json(jsonData);
+})
 /*Dependencies */
 const bodyParser = require('body-parser')
 
@@ -18,21 +41,15 @@ const cors=require('cors');
 app.use(cors());
 
 //Intialize the main project folder
-app.use(express.static('demo'));
+app.use(express.static('website'));
 
 const port=8000;
 //spin up the server
-const server = app.listen(port,listening);
+
 
 //
 const server = app.listen(port, listening);
 function listening(){
     // console.log(server);
     console.log(`running on localhost: ${port}`);
-};
-const data = [];
-app.post('/flavor', addFlavor);
-
-function addFlavor (req, res) {
-  data.push(req.body);
 };

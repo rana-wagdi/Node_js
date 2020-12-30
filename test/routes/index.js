32cbd22c1a21e25af6a9b2cfed80a7a4afec1e7c
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-12-30 09:17:52 -0800

    add HBS template

--- test/routes/index.js
+++ test/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RANA' });
+  res.render('index', { title: 'Express' });
 });
 router.get('/info',)
 module.exports = router;

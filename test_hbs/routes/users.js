88995b43dda30fd9b62efd3a20ed63f1d0ea625b
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-01 05:57:12 -0800

    get

--- test_hbs/routes/users.js
+++ test_hbs/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/info/:id',(req,res,next)=>{
+  console.log(req.params.id);
+  res.render('info')
+})
+
 module.exports = router;

63ebb39f30e9b3005b2b7bd912ea2f558ac54e1c
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-16 13:18:54 -0800

    NPM

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,3 +1,4 @@
-const fs = require('fs');
 
-fs.copyFileSync("text1.txt","text2.txt");
+var supervillains = require("supervillains");
+var mysupervillains = supervillains.random();
+console.log(mysupervillains);

3ceb1f08dfea081dc4e85876cb3e79ee80b93965
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-17 13:28:20 -0800

    routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,15 @@ const app =express();
 app.get("/", function(request,response){
 response.send("<h1>hello world!</h1>");
 });
+
+app.get('/contact', function(request, response){
+    response.send("Contact me at: ranawagdi@gmail.com")
+});
+
+app.get('/about', function(request, response){
+    response.send("<p>my name is Rana <br> I graduate 2020</p>")
+})
+
 app.listen(3000,function(){
     console.log("server started on port 3000")
 });
